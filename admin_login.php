3469ff08225d34cd41bf73c5e6eabf4fb5334c9c
Muhammad Umar Hatta <oemarhatta123@gmail.com>
2024-10-21 12:01:32 +0700

    Update admin_login.php

--- admin_login.php
+++ admin_login.php
@@ -22,7 +22,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <title>Admin Login</title>
-    <link rel="stylesheet" href="style2.css">
 </head>
 <body>
     <div class="form-container">
